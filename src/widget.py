d894565762c1c25e616c031683fc1e31b9c0cbd4
Yuriy Kurilenko <kurilenko816@gmail.com>
2026-08-17 22:41:46 +0700

    gitignor fixes

--- src/widget.py
+++ src/widget.py
@@ -1,2 +1,8 @@
-def mask_account_card(account_namber_card: str) -> str:
-    """ Создаем функцию которая умеет обрабатывать информацию как о картах, так и о счетах"""
+def mask_account_card(account_number_card: str) -> str:
+    """Создаем функцию которая умеет обрабатывать информацию как о картах, так и о счетах"""
+
+    return
+
+
+if __name__ == '__main__':
+    print()
