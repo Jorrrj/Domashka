0bb0daee48dda1e7a03aaed88df09895208a4940
Yuriy Kurilenko <kurilenko816@gmail.com>
2026-08-20 22:25:53 +0700

    update funk filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,16 @@
-def filter_by_state(list[]):
+def filter_by_state(user_list: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """Функция которая принимает список словарей и опционально значение для ключа
 state (по умолчанию 'EXECUTED').
  Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
 state соответствует указанному значению"""
-    pass
+    new_list = list()     # Переменная куда будут попадать отсортированные
+                          # данные по ключу state со значением EXECUTED
+
+    for item in user_list:     # Перебор словаря по ключу и значению
+        if item['state'] == state: # Ключ state по умолчанию = EXECUTED при совпадении добавляется в new_list
+            new_list.append(item)
+
+    return new_list
 
 
 
@@ -12,3 +19,11 @@ def sort_by_date(date):
      задающий порядок сортировки (по умолчанию — убывание).
      Функция должна возвращать новый список, отсортированный по дате (date)."""
     pass
+
+if __name__ == '__main__':
+    print(filter_by_state([
+    {'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+
