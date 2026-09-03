e9010d2195b5809e62fa46dbce8ffcbc46ffdf76
Yuriy Kurilenko <kurilenko816@gmail.com>
2026-09-04 02:25:06 +0700

    add tests

--- src/generators.py
+++ src/generators.py
@@ -20,14 +20,14 @@ def transaction_descriptions(list_of_dicts):
         for i in list_of_dicts:
             yield i.get("description")
     else:
-        yield {}
+        yield "Список пуст"
 
 
 def card_number_generator(start, stop):
     ''' Генератор должен принимать начальное и конечное значения для генерации диапазона номеров карт '''
     if isinstance(start, int) is True and isinstance(stop, int) is True:
         for number in range(start, stop + 1):
-            num_card = str(number).zfill(16)
+            num_card = str(number).zfill(16) # Функция которая добавлят нули слева
             yield num_card[0: 4] + " " + num_card[4: 8] + " " + num_card[8: 12] + " " + num_card[12: ]
     else:
         yield "Не верные данные"
